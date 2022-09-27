09c0b3d7acf37f071c3a1b283ac6a04835593c38
LemonMantis5571 <briter456@gmail.com>
2022-09-27 17:23:32 -0600

    fixing

--- js/app.js
+++ js/app.js
@@ -84,58 +84,62 @@
     }
 
     function obtenerClientes() {
-        const abrirConexion = window.indexedDB.open('crm', 1);
 
+        let abrirConexion = window.indexedDB.open('crm', 1);
 
-        abrirConexion.onerror = () =>{
-            console.log('hubo un error');
-        }
-
-        abrirConexion.onsuccess = () => {
-            console.log('se abrio la conexion');
-
+        // si hay un error, lanzarlo
+        abrirConexion.onerror = function() {
+            console.log('Hubo un error');
+        };
+    
+        // si todo esta bien, asignar a database el resultado
+        abrirConexion.onsuccess = function() {
+            // guardamos el resultado
             DB = abrirConexion.result;
 
             const objectStore = DB.transaction('crm').objectStore('crm');
 
-            objectStore.openCursor().onsuccess = function (e) {
 
-                const cursor  = e.target.result;
+            // retorna un objeto request o petición, 
+            objectStore.openCursor().onsuccess = function(e) {
+                 // cursor se va a ubicar en el registro indicado para accede ra los datos
+                 const cursor = e.target.result;
 
-                if(cursor) {
-                    const { nombre , empresa, email, telefono, id} = cursor.value;
-
+                //  console.log(e.target);
+     
+                 if(cursor) {
+                    const { nombre, empresa, email, telefono, id } = cursor.value;
                     
-
+                    const listadoClientes = document.querySelector('#listado-clientes');
                     listadoClientes.innerHTML += `
-                    <tr>
-                        <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
-                            <p class="text-sm leading-5 font-medium text-gray-700 text-lg  font-bold"> ${nombre} </p>
-                            <p class="text-sm leading-10 text-gray-700"> ${email} </p>
-                        </td>
-                        <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200 ">
-                            <p class="text-gray-700">${telefono}</p>
-                        </td>
-                        <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200  leading-5 text-gray-700">    
-                            <p class="text-gray-600">${empresa}</p>
-                        </td>
-                        <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200 text-sm leading-5">
-                            <a href="editar-cliente.html?id=${id}" class="text-teal-600 hover:text-teal-900 mr-5">Editar</a>
-                            <a href="#" data-cliente="${id}" class="text-red-600 hover:text-red-900 eliminar">Eliminar</a>
-                        </td>
-                    </tr>`;
-                    
+
+                        <tr>
+                            <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
+                                <p class="text-sm leading-5 font-medium text-gray-700 text-lg  font-bold"> ${nombre} </p>
+                                <p class="text-sm leading-10 text-gray-700"> ${email} </p>
+                            </td>
+                            <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200 ">
+                                <p class="text-gray-700">${telefono}</p>
+                            </td>
+                            <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200  leading-5 text-gray-700">    
+                                <p class="text-gray-600">${empresa}</p>
+                            </td>
+                            <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200 text-sm leading-5">
+                                <a href="editar-cliente.html?id=${id}" class="text-teal-600 hover:text-teal-900 mr-5">Editar</a>
+                                <a href="#" data-cliente="${id}" class="text-red-600 hover:text-red-900">Eliminar</a>
+                            </td>
+                        </tr>
+                    `;
+        
                     cursor.continue();
-                }
-    
-                else {
-                    console.log('no hay mas registros');
-                }
-            }
+                 } else {
+                    //  console.log('llegamos al final...');
+                 }
+             };
 
-            
+        };
 
 
-        }
     }
+   
 }) ();

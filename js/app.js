664420941b919e3bee9dca3bf064dea47b22ce23
LemonMantis5571 <briter456@gmail.com>
2022-09-27 17:29:14 -0600

    Finally Working as intended xD

--- js/app.js
+++ js/app.js
@@ -7,10 +7,7 @@
 
 
         if(window.indexedDB.open('crm', 1)){
-            setTimeout(() => {
                 obtenerClientes();
-            }, 5000);
-            
         }
 
         listadoClientes.addEventListener('click', eliminarRegistro);
@@ -126,7 +123,7 @@
                             </td>
                             <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200 text-sm leading-5">
                                 <a href="editar-cliente.html?id=${id}" class="text-teal-600 hover:text-teal-900 mr-5">Editar</a>
-                                <a href="#" data-cliente="${id}" class="text-red-600 hover:text-red-900">Eliminar</a>
+                                <a href="#" data-cliente="${id}" class="text-red-600 hover:text-red-900 eliminar">Eliminar</a>
                             </td>
                         </tr>
                     `;

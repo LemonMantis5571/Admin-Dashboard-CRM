9ede6d71c9e97b75dbaeaf20d087ebc7d9f89130
LemonMantis5571 <briter456@gmail.com>
2022-09-27 17:20:50 -0600

    Prueba

--- js/app.js
+++ js/app.js
@@ -46,31 +46,41 @@
     }
 
     function crearDB() {
+        // crear base de datos con la versión 1
         const crearDB = window.indexedDB.open('crm', 1);
-
-        crearDB.onerror = () => {
-            console.log("hubo un error")
+    
+        // si hay un error, lanzarlo
+        crearDB.onerror = function() {
+            console.log('Hubo un error');
         };
-
-
-        crearDB.onsuccess = () => {
-            console.log("Exito");
+    
+        // si todo esta bien, asignar a database el resultado
+        crearDB.onsuccess = function() {
+            // guardamos el resultado
             DB = crearDB.result;
         };
-
-        crearDB.onupgradeneeded = (e) => {
-            console.log("DB creada");
+    
+        // este método solo corre una vez
+        crearDB.onupgradeneeded = function(e) {
+            // el evento que se va a correr tomamos la base de datos
             const db = e.target.result;
-
-
-            const objectStore = db.createObjectStore('crm', {keypath: 'id', autoIncrement: true})
+    
             
-            objectStore.createIndex('nombre', 'nombre', {unique: false});
-            objectStore.createIndex('email', 'email', {unique: true});
-            objectStore.createIndex('telefono', 'telefono', {unique: false});
-            objectStore.createIndex('empresa', 'empresa', {unique: false});
-            objectStore.createIndex('id', 'id', {unique: true});
+            // definir el objectstore, primer parametro el nombre de la BD, segundo las opciones
+            // keypath es de donde se van a obtener los indices
+            const objectStore = db.createObjectStore('crm', { keyPath: 'id',  autoIncrement: true } );
+    
+            //createindex, nombre y keypath, 3ro los parametros
+            objectStore.createIndex('nombre', 'nombre', { unique: false } );
+            objectStore.createIndex('email', 'email', { unique: true } );
+            objectStore.createIndex('telefono', 'telefono', { unique: false } );
+            objectStore.createIndex('empresa', 'empresa', { unique: false } );
+            objectStore.createIndex('id', 'id', { unique: true } );
+    
+            
+            console.log('Database creada y lista');
         };
+    
     }
 
     function obtenerClientes() {
